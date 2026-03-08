7e4c158f0a5942afc86c4cbed3c700ee38e71aaa
it1shka <2tishbel@gmail.com>
2026-03-08 14:05:06 +0100

    feat(solver): added problem instance definition

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,10 @@
-use ratatui::{DefaultTerminal, Frame};
+use pfsp_solver::solver::problem;
+use std::io;
 
-fn main() -> color_eyre::Result<()> {
-    color_eyre::install()?;
-    ratatui::run(app)?;
-    Ok(())
-}
-
-fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
-    loop {
-        terminal.draw(render)?;
-        if crossterm::event::read()?.is_key_press() {
-            break Ok(());
-        }
-    }
-}
-
-fn render(frame: &mut Frame) {
-    frame.render_widget("hello world", frame.area());
+fn main() {
+    let problem = {
+        let handle = io::stdin().lock();
+        problem::parse_problem(handle)
+    };
+    println!("{:#?}", problem);
 }
---
+++ src/solver/problem.rs
@@ -0,0 +1,109 @@
+use std::io::{self, BufRead};
+
+pub type Time = u64;
+
+#[derive(Debug)]
+pub struct Problem {
+    jobs_number: usize,
+    machines_number: usize,
+    initial_seed: Option<u64>,
+    upper_bound: Option<Time>,
+    lower_bound: Option<Time>,
+    processing_times: Vec<Vec<Time>>,
+}
+
+pub fn parse_problem<R: io::Read>(source: R) -> io::Result<Problem> {
+    macro_rules! parser_err {
+        ($msg:expr) => {
+            io::Error::new(io::ErrorKind::InvalidData, $msg)
+        };
+    }
+
+    let reader = io::BufReader::new(source);
+    let mut lines = reader.lines();
+
+    lines
+        .next()
+        .ok_or(parser_err!("Parameters header line was expected"))??;
+    let raw_parameters = lines
+        .next()
+        .ok_or(parser_err!("Problem parameters were expected"))??;
+    let parameters = raw_parameters.split_whitespace().collect::<Vec<_>>();
+    if parameters.len() != 5 {
+        return Err(parser_err!("Expected exactly 5 problem parameters"));
+    }
+
+    macro_rules! required_param {
+        ($nth: expr, $type:ty, $err_msg:expr) => {
+            parameters[$nth]
+                .parse::<$type>()
+                .map_err(|_| parser_err!($err_msg))?
+        };
+    }
+    macro_rules! optional_param {
+        ($nth: expr, $type:ty, $err_msg:expr) => {
+            if parameters[$nth] == "-" {
+                None
+            } else {
+                Some(required_param!($nth, $type, $err_msg))
+            }
+        };
+    }
+
+    let jobs_number = required_param!(
+        0,
+        usize,
+        "Parameter for jobs number should be an unsigned integer"
+    );
+    let machines_number = required_param!(
+        1,
+        usize,
+        "Parameter for machines number should be an unsigned integer"
+    );
+    let initial_seed = optional_param!(
+        2,
+        u64,
+        "Parameter for initial seed should be an unsigned 64-bit integer or a hyphen"
+    );
+    let upper_bound = optional_param!(
+        3,
+        Time,
+        "Parameter for upper bound should be an unsigned integer or a hyphen"
+    );
+    let lower_bound = optional_param!(
+        4,
+        Time,
+        "Parameter for lower bound should be an unsigned integer or a hyphen"
+    );
+
+    lines
+        .next()
+        .ok_or(parser_err!("Processing times header line was expected"))??;
+    let mut processing_times: Vec<Vec<Time>> = Vec::with_capacity(machines_number);
+    for _ in 0..machines_number {
+        let raw_machine_processing_times = lines
+            .next()
+            .ok_or(parser_err!("Processing times line was expected"))??;
+        let machine_processing_times_or_err = raw_machine_processing_times
+            .split_whitespace()
+            .map(|processing_time| processing_time.parse::<Time>())
+            .collect::<Result<Vec<_>, _>>();
+        let machine_processing_times = machine_processing_times_or_err.map_err(|_| {
+            parser_err!("Processing times line should contain only unsigned integers")
+        })?;
+        if machine_processing_times.len() != jobs_number {
+            return Err(parser_err!(
+                "Processing times line has different width than the number of jobs"
+            ));
+        }
+        processing_times.push(machine_processing_times)
+    }
+    Ok(Problem {
+        jobs_number: jobs_number,
+        machines_number: machines_number,
+        initial_seed: initial_seed,
+        upper_bound: upper_bound,
+        lower_bound: lower_bound,
+        processing_times: processing_times,
+    })
+}
