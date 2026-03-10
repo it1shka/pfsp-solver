cf0e237707d13bcf56aa646d51e07d4ec63286ae
it1shka <2tishbel@gmail.com>
2026-03-10 14:34:44 +0100

    feat(tui): finished general TUI layout

--- src/tui/state.rs
+++ src/tui/state.rs
@@ -3,12 +3,43 @@ use pfsp_solver::solver::problem::Problem;
 pub struct AppState<'a> {
     pub problem: &'a Problem,
     pub is_running: bool,
+    pub screen: AppScreen,
     pub raw_solution: String,
+}
+
+#[derive(Clone, Copy, PartialEq)]
+pub enum AppScreen {
+    ProblemInstance,
+    CurrentSolution,
+    Algorithms,
+    ControlPanel,
+}
+
+impl AppScreen {
+    fn next_screen(&self) -> Self {
+        match self {
+            AppScreen::ProblemInstance => AppScreen::CurrentSolution,
+            AppScreen::CurrentSolution => AppScreen::Algorithms,
+            AppScreen::Algorithms => AppScreen::ControlPanel,
+            AppScreen::ControlPanel => AppScreen::ProblemInstance,
+        }
+    }
+
+    fn prev_screen(&self) -> Self {
+        match self {
+            AppScreen::ProblemInstance => AppScreen::ControlPanel,
+            AppScreen::CurrentSolution => AppScreen::ProblemInstance,
+            AppScreen::Algorithms => AppScreen::CurrentSolution,
+            AppScreen::ControlPanel => AppScreen::Algorithms,
+        }
+    }
 }
 
 #[derive(Clone, Copy)]
 pub enum AppEvent {
     Close,
+    PrevScreen,
+    NextScreen,
     DeleteSymbol,
     AddSymbol(char),
 }
@@ -18,19 +49,28 @@ impl<'a> AppState<'a> {
         AppState {
             problem: problem,
             is_running: true,
+            screen: AppScreen::ProblemInstance,
             raw_solution: String::new(),
         }
     }
 
     pub fn update_on_event(&mut self, event: AppEvent) {
         match event {
+            AppEvent::NextScreen => self.screen = self.screen.next_screen(),
+            AppEvent::PrevScreen => self.screen = self.screen.prev_screen(),
             AppEvent::Close => self.is_running = false,
-            AppEvent::DeleteSymbol => {
-                self.raw_solution.pop();
-            }
-            AppEvent::AddSymbol(symbol) => {
-                self.raw_solution.push(symbol);
-            }
+            _ => match self.screen {
+                AppScreen::CurrentSolution => match event {
+                    AppEvent::DeleteSymbol => {
+                        self.raw_solution.pop();
+                    }
+                    AppEvent::AddSymbol(symbol) => {
+                        self.raw_solution.push(symbol);
+                    }
+                    _ => {}
+                },
+                _ => {}
+            },
         }
     }
 }
--- src/tui/view.rs
+++ src/tui/view.rs
@@ -5,10 +5,11 @@ use ratatui::{
     Frame, Terminal,
     layout::{Constraint, Direction, Layout, Rect},
     prelude::Backend,
-    widgets::{Block, Borders, Paragraph},
+    style::{Color, Style},
+    widgets::{Block, Borders, List, ListItem, Paragraph},
 };
 
-use crate::tui::state::{AppEvent, AppState};
+use crate::tui::state::{AppEvent, AppScreen, AppState};
 
 const EVENT_POLL_TIME: u64 = 33;
 
@@ -38,11 +39,21 @@ fn read_app_event() -> io::Result<Option<AppEvent>> {
         Event::Key(key) => match key.code {
             KeyCode::Esc => Some(AppEvent::Close),
             KeyCode::Backspace => Some(AppEvent::DeleteSymbol),
+            KeyCode::Up => Some(AppEvent::PrevScreen),
+            KeyCode::Down => Some(AppEvent::NextScreen),
             _ => key.code.as_char().map(|symbol| AppEvent::AddSymbol(symbol)),
         },
         _ => None,
     };
     Ok(app_event)
+}
+
+fn style_for_screen(screen: AppScreen, state: &AppState) -> Style {
+    if screen == state.screen {
+        Style::default().fg(Color::Yellow)
+    } else {
+        Style::default()
+    }
 }
 
 fn render_frame(frame: &mut Frame, state: &AppState) {
@@ -58,69 +69,87 @@ fn render_sidebar(frame: &mut Frame, state: &AppState, rect: Rect) {
     let chunks = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
-            Constraint::Length(10),
+            Constraint::Length(7),
             Constraint::Length(3),
             Constraint::Min(0),
+            Constraint::Length(3),
         ])
         .split(rect);
     render_problem_description(frame, state, chunks[0]);
     render_solution_input(frame, state, chunks[1]);
+    render_algorithms(frame, state, chunks[2]);
+    render_control_panel(frame, state, chunks[3]);
 }
 
 fn render_problem_description(frame: &mut Frame, state: &AppState, rect: Rect) {
-    let block = Block::default()
-        .title("Problem Instance")
-        .borders(Borders::ALL);
-    frame.render_widget(&block, rect);
-    let chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([
-            Constraint::Length(1),
-            Constraint::Length(1),
-            Constraint::Length(1),
-            Constraint::Length(1),
-            Constraint::Length(1),
-        ])
-        .split(block.inner(rect));
-    let jobs_number_p = Paragraph::new(format!("Jobs: {}", state.problem.jobs_number));
-    frame.render_widget(jobs_number_p, chunks[0]);
-    let machines_number_p = Paragraph::new(format!("Machines: {}", state.problem.machines_number));
-    frame.render_widget(machines_number_p, chunks[1]);
-    let initial_seed_p = Paragraph::new(
-        state
-            .problem
-            .initial_seed
-            .map(|seed| format!("Seed: {}", seed))
-            .unwrap_or(String::from("Seed: -")),
+    let items = [
+        ListItem::new(format!("Jobs: {}", state.problem.jobs_number)),
+        ListItem::new(format!("Machines: {}", state.problem.machines_number)),
+        ListItem::new(
+            state
+                .problem
+                .initial_seed
+                .map(|seed| format!("Seed: {}", seed))
+                .unwrap_or(String::from("Seed: -")),
+        ),
+        ListItem::new(
+            state
+                .problem
+                .upper_bound
+                .map(|upper_bound| format!("Upper bound: {}", upper_bound))
+                .unwrap_or(String::from("Upper bound: -")),
+        ),
+        ListItem::new(
+            state
+                .problem
+                .lower_bound
+                .map(|lower_bound| format!("Lower bound: {}", lower_bound))
+                .unwrap_or(String::from("Lower bound: -")),
+        ),
+    ];
+    let list = List::new(items).block(
+        Block::default()
+            .title("Problem Instance")
+            .borders(Borders::ALL)
+            .border_style(style_for_screen(AppScreen::ProblemInstance, state)),
     );
-    frame.render_widget(initial_seed_p, chunks[2]);
-    let upper_bound_p = Paragraph::new(
-        state
-            .problem
-            .upper_bound
-            .map(|upper_bound| format!("Upper bound: {}", upper_bound))
-            .unwrap_or(String::from("Upper bound: -")),
-    );
-    frame.render_widget(upper_bound_p, chunks[3]);
-    let lower_bound_p = Paragraph::new(
-        state
-            .problem
-            .upper_bound
-            .map(|lower_bound| format!("Lower bound: {}", lower_bound))
-            .unwrap_or(String::from("Lower bound: -")),
-    );
-    frame.render_widget(lower_bound_p, chunks[4]);
+    frame.render_widget(list, rect);
 }
 
 fn render_solution_input(frame: &mut Frame, state: &AppState, rect: Rect) {
-    let input_p = Paragraph::new(state.raw_solution.clone()).block(
+    let mut input_value = state.raw_solution.clone();
+    if state.screen == AppScreen::CurrentSolution {
+        input_value.push('_');
+    }
+    let input_p = Paragraph::new(input_value).block(
         Block::default()
+            .title("Current Solution")
             .borders(Borders::ALL)
-            .title("Current Solution"),
+            .border_style(style_for_screen(AppScreen::CurrentSolution, state)),
     );
     frame.render_widget(input_p, rect);
 }
 
-fn render_main_panel(frame: &mut Frame, state: &AppState, rect: Rect) {
+fn render_algorithms(frame: &mut Frame, state: &AppState, rect: Rect) {
+    let block = Block::default()
+        .title("Algorithms")
+        .borders(Borders::ALL)
+        .border_style(style_for_screen(AppScreen::Algorithms, state));
     // TODO:
+    frame.render_widget(block, rect);
+}
+
+fn render_control_panel(frame: &mut Frame, state: &AppState, rect: Rect) {
+    let block = Block::default()
+        .title("Run algorithm")
+        .borders(Borders::ALL)
+        .border_style(style_for_screen(AppScreen::ControlPanel, state));
+    // TODO:
+    frame.render_widget(block, rect);
+}
+
+fn render_main_panel(frame: &mut Frame, state: &AppState, rect: Rect) {
+    let block = Block::default().borders(Borders::ALL);
+    // TODO:
+    frame.render_widget(block, rect);
 }
