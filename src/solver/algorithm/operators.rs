5b75ca59bf87daffe16e7b52ad432df12dc8b546
it1shka <2tishbel@gmail.com>
2026-03-17 14:18:44 +0100

    feat(solver): implemented OrderedCrossover and PartiallyMatchedCrossover

--- src/solver/algorithm/operators.rs
+++ src/solver/algorithm/operators.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use rand::{Rng, RngExt};
 
 use crate::solver::solution::Solution;
@@ -29,30 +31,50 @@ macro_rules! define_operator {
     };
 }
 
-define_operator!(SimpleCrossover);
-impl<R: Rng> BinaryOperator<R> for SimpleCrossover {
-    fn mutate(&self, rng: &mut R, s1: &mut Solution, s2: &Solution) {
-        let split = rng.random_range(0..s1.len());
-        let direction = rng.random_bool(0.5);
-        if direction {
-            s1.data[split..].copy_from_slice(&s2.data[split..]);
-        } else {
-            s1.data[..split].copy_from_slice(&s2.data[..split]);
-        }
-    }
-}
-
 define_operator!(OrderedCrossover);
 impl<R: Rng> BinaryOperator<R> for OrderedCrossover {
     fn mutate(&self, rng: &mut R, s1: &mut Solution, s2: &Solution) {
-        todo!()
+        let n = s1.len();
+        let p1 = rng.random_range(0..(n - 1));
+        let p2 = rng.random_range((p1 + 1)..n);
+        let mut present = vec![false; n];
+        for &v in &s1.data[p1..p2] {
+            present[v] = true;
+        }
+        let mut s2_ordered = s2.data[p2..]
+            .iter()
+            .copied()
+            .chain(s2.data[..p2].iter().copied())
+            .filter(|&v| !present[v]);
+        for v in s1.data[p2..].iter_mut() {
+            *v = s2_ordered.next().unwrap();
+        }
+        for v in s1.data[..p1].iter_mut() {
+            *v = s2_ordered.next().unwrap();
+        }
     }
 }
 
 define_operator!(PartiallyMatchedCrossover);
 impl<R: Rng> BinaryOperator<R> for PartiallyMatchedCrossover {
     fn mutate(&self, rng: &mut R, s1: &mut Solution, s2: &Solution) {
-        todo!()
+        let n = s1.len();
+        let p1 = rng.random_range(0..(n - 1));
+        let p2 = rng.random_range((p1 + 1)..n);
+
+        let mut mapping = vec![None; n];
+        for i in p1..p2 {
+            let from = s2.data[i];
+            let to = s1.data[i];
+            mapping[from] = Some(to);
+        }
+
+        s1.data[p1..p2].copy_from_slice(&s2.data[p1..p2]);
+        for i in (0..p1).chain(p2..n) {
+            while let Some(mapped) = mapping[s1.data[i]] {
+                s1.data[i] = mapped;
+            }
+        }
     }
 }
 
@@ -82,8 +104,6 @@ impl<R: Rng> UnaryOperator<R> for InversionMutation {
     fn mutate(&self, rng: &mut R, s1: &mut Solution) {
         let p1 = rng.random_range(0..s1.len() - 1);
         let p2 = rng.random_range((p1 + 1)..s1.len());
-        // let inversed = s1.data[p1..p2].into_iter
-        // s1.data[p1..p2].copy_from_slice
-        todo!()
+        s1.data[p1..p2].reverse();
     }
 }
