9be94fd772811e2cef6f2da9bb5f763dac3b649f
it1shka <2tishbel@gmail.com>
2026-03-21 19:07:55 +0100

    feat(solver): added ffe count to evaluator

--- src/solver/evaluator.rs
+++ src/solver/evaluator.rs
@@ -6,10 +6,12 @@ use std::{
 use crate::solver::{problem::Time, solution::Solution};
 
 pub trait Evaluator {
+    fn eval_count(&self) -> u64;
     fn evaluate(&mut self, solution: &Solution) -> Time;
 }
 
 pub struct TFTEvaluator<'a> {
+    unique_evaluations: u64,
     cache: HashMap<u64, Time>,
     processing_times: &'a [Vec<Time>],
 }
@@ -17,6 +19,7 @@ pub struct TFTEvaluator<'a> {
 impl<'a> TFTEvaluator<'a> {
     pub fn new(processing_times: &'a [Vec<Time>]) -> Self {
         Self {
+            unique_evaluations: 0,
             cache: HashMap::new(),
             processing_times,
         }
@@ -28,13 +31,17 @@ impl<'a> TFTEvaluator<'a> {
 }
 
 impl<'a> Evaluator for TFTEvaluator<'a> {
+    fn eval_count(&self) -> u64 {
+        self.unique_evaluations
+    }
+
     fn evaluate(&mut self, solution: &Solution) -> Time {
         let mut hasher = DefaultHasher::new();
         solution.hash(&mut hasher);
         let key = hasher.finish();
-        *self
-            .cache
-            .entry(key)
-            .or_insert_with(|| solution.total_flow_time(self.processing_times))
+        *self.cache.entry(key).or_insert_with(|| {
+            self.unique_evaluations += 1;
+            solution.total_flow_time(self.processing_times)
+        })
     }
 }
